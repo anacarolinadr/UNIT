9ae4c50b238d296250dd049a02a64fae4a64f209
AnaCarolina <passoscarol15@gmail.com>
2025-02-19 11:05:49 -0300

    Exercicio 2 - Lab de Programação

--- laboratorio-de-programaçao/1- slide-revisao-python/exercicio-2.py
+++ laboratorio-de-programaçao/1- slide-revisao-python/exercicio-2.py
@@ -20,8 +20,80 @@
 # e) Imprimir (em ordem crescente) as dezenas da primeira aposta, 
 # das duas apostas (surpresinha) e do resultado do concurso da Lotofácil simulado.
 
+import random
+
 class Lotofacil:
-    ...
+    
+    def __init__(self):
+        self.qtd_dezenas = 0
+        self.nums_aposta = []
+        self.surpresinhas = []
+        self.nums_sorteio = []
+    
+    def escolher_quantidade_dezenas(self):
+        while True:
+            qtd_dezenas = input("Digite a quantidade de dezenas desejada: (15 a 18) ")
+
+            try:
+                qtd_dezenas = int(qtd_dezenas)
+                if qtd_dezenas >= 15 and qtd_dezenas <= 18:
+                    self.qtd_dezenas = qtd_dezenas
+                    return self.qtd_dezenas
+                else:
+                    print("Valor inválido para quantidade, digite um valor entre 15 a 18")
+
+            except ValueError:
+                print("Valor inválido para quantidade, digite um número")
+    
+    def escolher_nums_aposta(self):
+        while len(self.nums_aposta) < self.qtd_dezenas:
+
+            print(f"Número {len(self.nums_aposta) + 1}")
+            num_aposta = input("Digite um número para sortear: (1 a 25) ")
+
+            try:
+                num_aposta = int(num_aposta)
+
+                if num_aposta in self.nums_aposta:
+                    print("Número repetido")
+                    continue
+                elif 1 <= num_aposta <= 25:
+                    self.nums_aposta.append(num_aposta)
+                else:
+                    print("Dezena inválida, digite um número entre 1 a 25 inteiro")
+
+            except ValueError:
+                print("Valor inválido para número, digite um valor numérico")
+
+        self.nums_aposta.sort() 
+
+    def gerar_surpresinhas(self):
+        for _ in range(2):
+            surpresinha = sorted(random.sample(range(1, 26), k=18))
+            self.surpresinhas.append(surpresinha)
+        return self.surpresinhas
+    
+    def gerar_sorteio(self):
+        self.nums_sorteio = sorted(random.sample(range(1, 26), k=15))
+        return self.nums_sorteio
+    
+    def imprimir_resultados(self):
+        print(f"\nRESULTADO SORTEIO")
+        print(f"Seus {self.qtd_dezenas} números escolhidos: \n{self.nums_aposta}")
+        print(f"Suas surpresinhas foram:\n1- {self.surpresinhas[0]}\n2- {self.surpresinhas[1]}")
+        print(f"Os números sorteados foram:\n{self.nums_sorteio}")
 
 def main():
-    ...
+    
+    lotofacil = Lotofacil()
+
+    print("\nBem-vindo à Loteria!")
+    lotofacil.escolher_quantidade_dezenas()
+    lotofacil.escolher_nums_aposta()
+    lotofacil.gerar_surpresinhas()
+    lotofacil.gerar_sorteio()
+    lotofacil.imprimir_resultados()
+
+
+if __name__ == "__main__":
+    main()
