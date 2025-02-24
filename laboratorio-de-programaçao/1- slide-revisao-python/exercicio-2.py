874271dc58a6a5c2df76b652116bd6cd64ff4426
AnaCarolina <passoscarol15@gmail.com>
2025-02-24 11:46:59 -0300

    Correção do Ex 2 - Lab de Programção

--- laboratorio-de-programaçao/1- slide-revisao-python/exercicio-2.py
+++ laboratorio-de-programaçao/1- slide-revisao-python/exercicio-2.py
@@ -22,78 +22,110 @@
 
 import random
 
+# Camada Domínio
+class ValoresLotofacil:
+    def __init__(self, qtd_dezenas, nums_aposta):
+        self.qtd_dezenas = qtd_dezenas
+        self.nums_aposta = nums_aposta
+
+# Camada de Serviço
 class Lotofacil:
-    
     def __init__(self):
         self.qtd_dezenas = 0
         self.nums_aposta = []
         self.surpresinhas = []
         self.nums_sorteio = []
-    
-    def escolher_quantidade_dezenas(self):
-        while True:
-            qtd_dezenas = input("Digite a quantidade de dezenas desejada: (15 a 18) ")
+        self.acertos_aposta = []
+        self.acertos_surpresinha1 = []
+        self.acertos_surpresinha2 = []
 
-            try:
-                qtd_dezenas = int(qtd_dezenas)
-                if qtd_dezenas >= 15 and qtd_dezenas <= 18:
-                    self.qtd_dezenas = qtd_dezenas
-                    return self.qtd_dezenas
-                else:
-                    print("Valor inválido para quantidade, digite um valor entre 15 a 18")
-
-            except ValueError:
-                print("Valor inválido para quantidade, digite um número")
-    
-    def escolher_nums_aposta(self):
-        while len(self.nums_aposta) < self.qtd_dezenas:
-
-            print(f"Número {len(self.nums_aposta) + 1}")
-            num_aposta = input("Digite um número para sortear: (1 a 25) ")
-
-            try:
-                num_aposta = int(num_aposta)
-
-                if num_aposta in self.nums_aposta:
-                    print("Número repetido")
-                    continue
-                elif 1 <= num_aposta <= 25:
-                    self.nums_aposta.append(num_aposta)
-                else:
-                    print("Dezena inválida, digite um número entre 1 a 25 inteiro")
-
-            except ValueError:
-                print("Valor inválido para número, digite um valor numérico")
-
-        self.nums_aposta.sort() 
+    def definir_aposta(self, valores_lotofacil):
+        self.qtd_dezenas = valores_lotofacil.qtd_dezenas
+        self.nums_aposta = valores_lotofacil.nums_aposta
 
     def gerar_surpresinhas(self):
         for _ in range(2):
             surpresinha = sorted(random.sample(range(1, 26), k=18))
             self.surpresinhas.append(surpresinha)
         return self.surpresinhas
-    
+
     def gerar_sorteio(self):
         self.nums_sorteio = sorted(random.sample(range(1, 26), k=15))
         return self.nums_sorteio
+
+    def checar_acertos(self):
+        nums_aposta_set = set(self.nums_aposta)
+        nums_sorteio_set = set(self.nums_sorteio)
+        surpresinha1_set = set(self.surpresinhas[0])
+        surpresinha2_set = set(self.surpresinhas[1])
+        
+        self.acertos_aposta = nums_aposta_set.intersection(nums_sorteio_set)
+        self.acertos_surpresinha1 = surpresinha1_set.intersection(nums_sorteio_set)
+        self.acertos_surpresinha2 = surpresinha2_set.intersection(nums_sorteio_set)
+        
+        return self.acertos_aposta, self.acertos_surpresinha1, self.acertos_surpresinha2
+
+# Camada de Interface: entrada e saída de dados
+class InterfaceLotofacil:
+    @staticmethod
+    def solicitar_dados():  
+        while True:
+            qtd_dezenas = input("Digite a quantidade de dezenas desejada: (15 a 18) ")
+
+            try:
+                qtd_dezenas = int(qtd_dezenas)
+                if qtd_dezenas >= 15 and qtd_dezenas <= 18:
+                    break
+                else:
+                    print("Valor inválido para quantidade, digite um valor entre 15 a 18")
+
+            except ValueError:
+                print("Valor inválido para quantidade, digite um número")
+
+        nums_aposta = []
+        while len(nums_aposta) < qtd_dezenas:
+            print(f"Número {len(nums_aposta) + 1}")
+            num_aposta = input("Digite um número para sortear: (1 a 25) ")
+
+            try:
+                num_aposta = int(num_aposta)
+
+                if num_aposta in nums_aposta:
+                    print("Número repetido")
+                    continue
+                elif 1 <= num_aposta <= 25:
+                    nums_aposta.append(num_aposta)
+                else:
+                    print("Dezena inválida, digite um número entre 1 a 25 inteiro")
+
+            except ValueError:
+                print("Valor inválido para número, digite um valor numérico")
+
+        nums_aposta.sort() 
+        return ValoresLotofacil(qtd_dezenas, nums_aposta)
     
+    @staticmethod
     def imprimir_resultados(self):
         print(f"\nRESULTADO SORTEIO")
         print(f"Seus {self.qtd_dezenas} números escolhidos: \n{self.nums_aposta}")
         print(f"Suas surpresinhas foram:\n1- {self.surpresinhas[0]}\n2- {self.surpresinhas[1]}")
         print(f"Os números sorteados foram:\n{self.nums_sorteio}")
+        print(f"Os números certos da sua aposta foram: \n{self.acertos_aposta}")
+        print(f"Os números certos da surpresinha 1 foram: \n{self.acertos_surpresinha1}")
+        print(f"Os números certos da surpresinha foram: \n{self.acertos_surpresinha2}")
 
+# Camada de Apresentação
 def main():
-    
+    interface = InterfaceLotofacil()
     lotofacil = Lotofacil()
 
     print("\nBem-vindo à Loteria!")
-    lotofacil.escolher_quantidade_dezenas()
-    lotofacil.escolher_nums_aposta()
+    valores = interface.solicitar_dados()
+    lotofacil.definir_aposta(valores)
     lotofacil.gerar_surpresinhas()
     lotofacil.gerar_sorteio()
-    lotofacil.imprimir_resultados()
-
+    lotofacil.checar_acertos()
+    interface.imprimir_resultados(lotofacil)
 
 if __name__ == "__main__":
     main()
